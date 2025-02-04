b0a59af8e40bb8c08ddad2197c3c442237f4bc10
King1923 <140989283+King1923@users.noreply.github.com>
2025-02-04 14:38:44 +0800

    Updated Cart.cs model

--- LearningAPI/Models/Cart.cs
+++ LearningAPI/Models/Cart.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text.Json.Serialization;
 
 namespace LearningAPI.Models
 {
@@ -9,10 +10,27 @@ namespace LearningAPI.Models
 		[Key]
 		public int CartId { get; set; }
 
+		// Foreign key for User
+		[Required]
+		public int UserId { get; set; }
+
+		// Foreign key for Product
+		[Required]
+		public int ProductId { get; set; }
+
 		[Required]
 		public int Quantity { get; set; }
 
 		[Required, Column(TypeName = "decimal(10,2)")]
 		public decimal TotalAmount { get; set; }
+
+		// Navigation properties
+		[JsonIgnore]
+		[ForeignKey("UserId")]
+		public User? User { get; set; }
+
+		[JsonIgnore]
+		[ForeignKey("ProductId")]
+		public Product? Product { get; set; }
 	}
 }
